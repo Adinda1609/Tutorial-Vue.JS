66ffe93d3addef4fe85c4029284221f98ec5a83f
SRI ADINDA <sriadinda@student.uir.ac.id>
2025-03-17 22:46:24 +0700

    first commit

---
+++ TUGAS4/src/components/ConditionalRendering.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const awesome = ref(true)
+
+function toggle() {
+  awesome.value = !awesome.value
+}
+</script>
+
+<template>
+    <button @click="toggle">Toggle</button>
+  <h1 v-if="awesome">SANGAT MENYENANGKAN!</h1>
+  <h1 v-else>Oh TIDAKKK😢</h1>
+</template>
+
+<!-- SRI ADINDA_233510515 -->
---
+++ TUGAS4/src/components/interpolationn.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref} from 'vue'
+
+const greeting = ref('Welcome to Vue!')
+const name = ref ('Sri Adinda')
+</script>
+
+<template>
+    <h1>{{ greeting }} {{ name }}</h1>
+</template>
+
+<!-- SRI ADINDA_233510515 -->
